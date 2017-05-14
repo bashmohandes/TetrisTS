76dc6a4da3366c46700b50a190d6c87c0ee6f433
Mohamed Elsherif <Bashmohandes@live.com>
2017-05-14 02:45:35 -0700

    Created first canvas

--- src/bundle.js
+++ src/bundle.js
@@ -2,8 +2,10 @@ var cell;
 function setup() {
     console.log("Hello");
     cell = new Cell(12, 13);
+    var canvas = createCanvas(400, 800);
 }
 function draw() {
+    background("BLACK");
     //console.log("Update");
     //console.log(cell);
 }
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ var cell : Cell;
 function setup() {
     console.log("Hello");
     cell = new Cell(12, 13);
+    var canvas = createCanvas(400, 800);
 }
 
 function draw() {
+    background("BLACK");
     //console.log("Update");
     //console.log(cell);
 }
